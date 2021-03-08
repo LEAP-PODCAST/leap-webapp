c947dbf3711a048345d22802fd2604ea73332082
Tryston Perry <TrystonMPerry@gmail.com>
2021-03-08 10:56:12 -0800

    Removed local webcam

--- src/routes/Room.vue
+++ src/routes/Room.vue
@@ -6,46 +6,6 @@
         style="height: calc(100% - 56px)"
       >
         <div
-          v-if="!webcams"
-          class="flex items-center justify-center w-full h-full text-gray-300"
-        >
-          <div class="w-full max-w-lg">
-            <h1 class="text-2xl mb-6 leading-none">
-              Welcome
-              <span class="font-bold">{{ room.username }}</span>
-              , here are some quick actions:
-            </h1>
-            <ul>
-              <li v-if="!room.localStreams.mic" class="flex items-center mb-2">
-                <d-btn
-                  @click="$store.dispatch('room/produceMic')"
-                  variant="none"
-                  class="p-btn inline-block rounded font-bold bg-dark-2 px-3 py-2"
-                >
-                  <span class="material-icons mr-2">mic</span>
-                  Capture Mic
-                </d-btn>
-                <span class="ml-2">Speak your mind.</span>
-              </li>
-              <li
-                v-if="!room.localStreams.webcam"
-                class="flex items-center mb-2"
-              >
-                <d-btn
-                  @click="$store.dispatch('room/produceWebcam')"
-                  variant="none"
-                  class="p-btn inline-block rounded font-bold bg-dark-2 px-3 py-2"
-                >
-                  <span class="material-icons mr-2">videocam</span>
-                  Capture Webcam
-                </d-btn>
-                <span class="ml-2">Match a face to your voice.</span>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div
-          v-else
           class="flex flex-wrap md:flex-row items-center justify-center w-full h-full"
         >
           <VideoContainer class="w-full h-full">
@@ -54,7 +14,6 @@
               :key="stream.producerId"
               class="video"
             >
-              <LocalWebcam v-if="room.localStreams.webcam" />
               <RemoteWebcam :stream="stream" />
             </div>
           </VideoContainer>
@@ -78,7 +37,6 @@
 
 <script>
 import VideoContainer from "@/components/VideoContainer";
-import LocalWebcam from "@/components/stream/LocalWebcam";
 import RemoteWebcam from "@/components/stream/RemoteWebcam";
 import VoiceChatter from "@/components/stream/VoiceChatter";
 import Controls from "@/components/stream/Controls";
@@ -87,7 +45,6 @@ import ChatRoom from "@/components/ChatRoom";
 export default {
   components: {
     VideoContainer,
-    LocalWebcam,
     RemoteWebcam,
     VoiceChatter,
     Controls,
