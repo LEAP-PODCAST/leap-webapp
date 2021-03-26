98ae3adbd7d8f2fe9c0708d802bc62f02b093ee1
Tryston Perry <trystonmperry@gmail.com>
2021-03-26 16:28:10 -0700

    Added dynamic welcome text

--- src/components/home/CreateRoom.vue
+++ src/components/home/CreateRoom.vue
@@ -2,28 +2,45 @@
   <div
     class="flex flex-col justify-center align-center p-8 w-full h-full text-center"
   >
-    <div>
-      <span class="text-sm font-thin opacity-75">
-        Good morning, {{ $store.state.user.userProfile.firstName }}!
-      </span>
-      <h2 class="text-2xl">
-        What would you like to do today?
-      </h2>
+    <div v-if="step === 0">
+      <div>
+        <span class="text-sm font-thin opacity-75">
+          Good {{ timeOfDayText }},
+          {{ $store.state.user.userProfile.firstName }}!
+        </span>
+        <h2 class="text-2xl">
+          What would you like to do today?
+        </h2>
+      </div>
+      <div class="flex flex-col items-center mt-8">
+        <d-btn variant="primary">
+          New episode
+        </d-btn>
+        <small class="italic text-xs opacity-50 my-2">or</small>
+        <d-btn @click="createRoom" variant="primary-outline">
+          Create room
+        </d-btn>
+      </div>
     </div>
-    <div class="flex flex-col items-center mt-8">
-      <d-btn variant="primary">
-        New episode
-      </d-btn>
-      <small class="italic text-xs opacity-50 my-2">or</small>
-      <d-btn @click="createRoom" variant="primary-outline">
-        Create room
-      </d-btn>
-    </div>
+    <div v-else-if="step === 1"></div>
   </div>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    step: 0
+  }),
+
+  computed: {
+    timeOfDayText() {
+      const hours = new Date().getHours();
+      if (hours > 3 && hours < 13) return "morning";
+      if (hours > 12 && hours < 18) return "aftenoon";
+      return "evening";
+    }
+  },
+
   methods: {
     async createRoom() {
       // TODO this will be set on the server side
