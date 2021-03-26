4e8dadf75a95cc939da794ffadbb8ee26b67e502
Tryston Perry <TrystonMPerry@gmail.com>
2021-03-25 21:01:53 -0700

    Added /login route

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ Vue.use(VueRouter);
 import Home from "@/routes/Home";
 import Room from "@/routes/Room";
 import SignUp from "@/routes/SignUp";
+import LogIn from "@/routes/LogIn";
 
 export default new VueRouter({
   mode: "history",
@@ -19,6 +20,11 @@ export default new VueRouter({
       path: "/signup",
       name: "Sign Up",
       component: SignUp
+    },
+    {
+      path: "/login",
+      name: "Log In",
+      component: LogIn
     },
     {
       path: "/:roomId",
