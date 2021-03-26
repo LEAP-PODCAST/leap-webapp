0bb22b02f341d22f1a27d9e489e1dd83d64e6eb9
Tryston Perry <TrystonMPerry@gmail.com>
2021-03-25 21:00:27 -0700

    Added signup page

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ Vue.use(VueRouter);
 
 import Home from "@/routes/Home";
 import Room from "@/routes/Room";
+import SignUp from "@/routes/SignUp";
 
 export default new VueRouter({
   mode: "history",
@@ -13,6 +14,11 @@ export default new VueRouter({
       path: "/",
       name: "Home",
       component: Home
+    },
+    {
+      path: "/signup",
+      name: "Sign Up",
+      component: SignUp
     },
     {
       path: "/:roomId",
