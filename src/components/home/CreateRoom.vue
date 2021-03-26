75f86259c9bfda3a2712bc8ddf3db6a8206d2296
Tryston Perry <trystonmperry@gmail.com>
2021-03-26 12:11:11 -0700

    Reformatted profile layout

--- src/components/home/CreateRoom.vue
+++ src/components/home/CreateRoom.vue
@@ -1,19 +1,46 @@
 <template>
-  <div class="flex flex-col justify-center align-center border">
-      <!-- Good Morning {{user.name}}! -->
-      Good Morning!
-      What would you like to do today?
-      <button @click="$emit('toggle-schedule')">New Episode</button>
-      <button>Upload Video</button>
+  <div
+    class="flex flex-col justify-center align-center p-8 w-full h-full text-center"
+  >
+    <div>
+      <span class="text-sm font-thin opacity-75">
+        Good morning, {{ $store.state.user.userProfile.firstName }}!
+      </span>
+      <h2 class="text-2xl">
+        What would you like to do today?
+      </h2>
+    </div>
+    <div class="flex flex-col items-center mt-8">
+      <d-btn variant="primary">
+        New episode
+      </d-btn>
+      <small class="italic text-xs opacity-50 my-2">or</small>
+      <d-btn variant="primary-outline">
+        Create room
+      </d-btn>
+    </div>
   </div>
 </template>
 
 <script>
-
-export default{
-}
+export default {
+  methods: {
+    async createRoom() {
+      console.log("createRoom");
+      if (!this.name) return;
+      // TODO this will be set on the server side
+      const roomId = Math.random()
+        .toString(36)
+        .substr(2, 9);
+      const res = await this.$store.dispatch("room/join", {
+        roomId,
+        username: this.name
+      });
+      if (!res) return;
+      this.$router.push(`/${roomId}`);
+    }
+  }
+};
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,51 +1,37 @@
 <template>
-  <div class="flex">
-    <div class="flex flex-col">
-      <Profile></Profile>
+  <div class="flex w-full h-full">
+    <div class="w-full h-full md:w-5/12 p-2">
+      <Profile
+        :profile="$store.state.user.userProfile"
+        class="display-card"
+      ></Profile>
     </div>
-    <CreateRoom v-if="schedule" @toggle-schedule="toggleSchedule"></CreateRoom>
-    <Schedule v-else @create-room="createRoom"></Schedule>
+    <div class="p-2 w-full h-full md:w-7/12">
+      <div class="display-card">
+        <CreateRoom @toggle-schedule="toggleSchedule"></CreateRoom>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Profile from "@/components/home/Profile";
 import CreateRoom from "@/components/home/CreateRoom";
-import Schedule from "@/components/home/Schedule";
 
 export default {
   components: {
     CreateRoom,
-    Profile,
-    Schedule
-  },
-
-  data: () => ({
-    schedule: true,
-    name: "Akif"
-  }),
-
-  methods: {
-    toggleSchedule() {
-      this.schedule = !this.schedule;
-    },
-
-    async createRoom() {
-      console.log("createRoom");
-      if (!this.name) return;
-      // TODO this will be set on the server side
-      const roomId = Math.random()
-        .toString(36)
-        .substr(2, 9);
-      const res = await this.$store.dispatch("room/join", {
-        roomId,
-        username: this.name
-      });
-      if (!res) return;
-      this.$router.push(`/${roomId}`);
-    }
+    Profile
   }
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.display-card {
+  @apply .text-white;
+  @apply .bg-black-800;
+  @apply .w-full;
+  @apply .h-full;
+  @apply .rounded-lg;
+}
+</style>
