c30ae0332f09c7e698447e3455eda5cefd56ab1a
Akif Manzoor <akifmanzoor124@gmail.com>
2021-04-07 11:40:19 -0400

    created cancel button for reset password redirect to /login

--- src/routes/PasswordReset.vue
+++ src/routes/PasswordReset.vue
@@ -1,31 +1,43 @@
-<template> 
+<template>
   <div
     class="p-8 bg-gray-900 text-white rounded-lg m-auto"
     style="max-width:600px"
   >
-  <h1 class="text-2xl font-bold mb-4">Reset Password</h1>
+    <h1 class="text-2xl font-bold mb-4">Reset Password</h1>
     <form>
       <div class="mb-3">
         <d-input
-            type="text"
-            v-model="username"
-            @blur="verifyUsername"
-            placeholder='Email'
-            required
+          type="text"
+          v-model="username"
+          @blur="verifyUsername"
+          placeholder="Email"
+          required
         />
-      </div> 
+      </div>
 
       <d-btn type="submit" variant="primary" class="w-full mt-6">
         Reset Password
       </d-btn>
+      <d-btn
+        type="submit"
+        variant="primary-outline"
+        class="w-full mt-3"
+        @click="returnToLogin()"
+      >
+        Cancel
+      </d-btn>
     </form>
-
   </div>
-
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    returnToLogin() {
+      this.$router.push("/login");
+    }
+  }
+};
 </script>
 
 <style></style>
