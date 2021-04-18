a72657f723c6ee7f6898bdb48bb41337a585fafb
Akif Manzoor <akifmanzoor124@gmail.com>
2021-04-18 11:32:57 -0400

    updated the landing page to look like figmas

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,46 +1,73 @@
 <template>
-  <div v-if="!waitlist" class="flex w-full h-full text-white scale-150">
-    <div class="hidden md:block md:w-1/12 bg-gray-900"></div>
-    <div
-      class="w-full flex justify-center md:justify-start md:items-center md:w-6/12 bg-gray-900"
-    >
-      <div class="p-6 md:ml-6" style="max-width:450px">
-        <img src="/images/leap.png" alt="Leap logo" class="mb-6" />
-        <h1 class="text-2xl font-bold py-6">
-          Effortlessly create, monetize, discover, go live, and interact with
-          your audience.
-        </h1>
+  <div class="flex flex-col w-full h-full p-6 pt-0 bg-gray-900">
+    <header class="flex flex-row items-center p-4">
+      <router-link to="/" style="display:contents;">
+        <img src="/images/leap.png" class="h-full" alt="Leap Logo" />
+      </router-link>
+      <div class="flex-grow"></div>
+    </header>
 
-        <div v-if="!waitlist">
-          <d-btn
-            variant="primary"
-            @click="waitlist = !waitlist"
-            class="w-full mt-5"
-          >
-            Request Access
-          </d-btn>
-          <d-btn
-            variant="primary-outline"
-            @click="$router.push('/LogIn')"
-            class="w-full mt-5"
-          >
-            Log In
-          </d-btn>
+    <div class="flex w-full h-full text-white bg-gray-900">
+      <div class="hidden md:block md:w-2/12 bg-gray-900"></div>
+      <div
+        class="w-full flex justify-center md:justify-start md:items-center md:w-6/12 bg-gray-900"
+      >
+        <div class="p-6 md:ml-6" style="max-width:450px">
+          <h1 class="text-6xl font-bold font-oxygen">
+            Simplify
+          </h1>
+          <h1 class="text-6xl font-bold font-oxygen leading-8">
+            Podcasting
+          </h1>
+          <h1 class="text-xl py-6">
+            Effortlessly record, monetize, discover, stream, and interact with
+            your audience.
+          </h1>
+
+          <div v-if="!waitlist">
+            <d-btn
+              variant="primary"
+              @click="waitlist = !waitlist"
+              class="w-full mt-5"
+            >
+              Join the Waitlist
+            </d-btn>
+            <d-btn
+              variant="primary-outline"
+              @click="$router.push('/LogIn')"
+              class="w-full mt-5"
+            >
+              Log In
+            </d-btn>
+          </div>
+          <div v-if="waitlist">
+            <form class="flex flex-col" @submit.prevent="submitEmail">
+              <d-input
+                type="text"
+                v-model="email"
+                placeholder="Email"
+                required
+              />
+              <div class="flex space-x-4 mt-2">
+                <d-btn variant="primary" type="submit" class="w-4/12"
+                  >Submit</d-btn
+                >
+                <d-btn
+                  @click="waitlist = !waitlist"
+                  variant="simple"
+                  class="w-4/12"
+                  >Cancel
+                </d-btn>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="hidden md:block md:w-6/12 bg-gray-900"></div>
-  </div>
 
-  <div v-else>
-    <iframe
-      id="typeform-full"
-      width="100%"
-      height="100%"
-      frameborder="0"
-      allow="camera; microphone; autoplay; encrypted-media;"
-      src="https://form.typeform.com/to/VaXJD5AB?typeform-medium=embed-snippet"
-    ></iframe>
+      <div class="flex justify-center items-center">
+        <img src="/images/valueprop.png" class="w-3/4" alt="Leap Logo" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -61,12 +88,7 @@ export default {
 </script>
 
 <style type="text/css">
-iframe {
-  position: absolute;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  top: 0;
-  border: 0;
+header {
+  max-height: 4rem;
 }
 </style>
