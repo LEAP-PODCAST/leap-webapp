dd5a484f0a1166396a6a1a33951ef558aa1a6091
Akif Manzoor <akifmanzoor124@gmail.com>
2021-05-06 12:19:10 -0400

    added page for 404

---
+++ src/routes/PageNotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="p-8 bg-gray-900 text-white w-full h-full flex flex-col items-center justify-center"
+  >
+    <p class="text-lg">
+      Ruh Oh! I don't know how you found yourself here! &nbsp;
+    </p>
+    <p>Quickly! to the nearest safe zone!</p>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss"></style>
